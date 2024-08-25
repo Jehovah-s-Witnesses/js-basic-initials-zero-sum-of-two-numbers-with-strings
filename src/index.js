4e713c416b724be718efa6c80cb275d68374ef69
gribVolnushka <burgerqueen98@gmail.com>
2024-08-25 13:57:59 +0200

    I got from user two numbers, and calculated sum of them and informed user about result

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const firstNumber = +prompt('Enter your first number');
+const secondNumber = +prompt('Enter your second number');
+let sum = firstNumber + secondNumber;
+
+alert(`Your first number is ${firstNumber}, your second number is ${secondNumber}. Result of adding these two numbers is ${sum}.`);
